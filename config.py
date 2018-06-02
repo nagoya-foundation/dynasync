3c842965df643e2e6c1fc654a6d5eb8a434cb252
blmayer <bleemayer@gmail.com>
2018-06-02 18:17:55 +0000

    Added config script.

---
+++ config.py
@@ -0,0 +1,40 @@
+""" 
+    PROJECT: dynasync: An utility that uses AWS's DynamoDB to store your 
+    files.
+    
+    AUTHOR: Brian Mayer
+    
+    DESCRIPTION: Configuration file for dynasync. Here we store the client's
+    preferences, such as remote db name and tracked directories. 
+"""
+
+import pathlib
+import json
+
+# Now we create the .config directory if needed
+config_dir = pathlib.Path().home() / ".config/dynasync"
+if not config_dir.exists():
+    config_dir.mkdir(parents=True)
+    print(".config/dynasync directory created.")
+
+# Check if config file exists in that dir
+config_file = config_dir / "dynasync.conf"
+configs = {}
+if config_file.exists():
+    print("Config file already exists.")
+    with open(config_file, 'r') as file:
+        configs = json.load(file)
+else:
+    # Create the config file
+    config_file.touch()
+    print("dynasync.conf file created.")
+
+    # Get configuration parameters from user and write to file
+    configs['dir'] = input("Enter directory to track:")
+    configs['table'] = input("Enter DynamoDB table name:")
+
+    with open(config_file, 'w') as file:
+        file.write(json.dumps(configs))
+
+print("Configuration finished!")
+
