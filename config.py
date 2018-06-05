05870c9b572160853e8caaab7c0e78a3490da45c
blmayer <bleemayer@gmail.com>
2018-06-05 02:49:56 +0000

    Improved paths.

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ import pathlib
 import json
 
 # Now we create the .config directory if needed
-config_dir = pathlib.Path().home() / ".config/dynasync"
+config_dir = pathlib.Path().home().joinpath(".config/dynasync")
 if not config_dir.exists():
     config_dir.mkdir(parents=True)
     print(".config/dynasync directory created.")
 
 # Check if config file exists in that dir
-config_file = config_dir / "dynasync.conf"
+config_file = config_dir.joinpath("dynasync.conf")
 configs = {}
 if config_file.exists():
     print("Config file already exists.")
