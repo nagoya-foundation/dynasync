2ea3c14c7b770c5b5a8f31176f6f8a2b6edc7bca
blmayer <bleemayer@gmail.com>
2018-06-09 01:45:23 +0000

    Switched from pathlib to os.

--- config.py
+++ config.py
@@ -1,34 +1,30 @@
-""" 
-    PROJECT: dynasync: An utility that uses AWS's DynamoDB to store your 
+"""
+    PROJECT: dynasync: An utility that uses AWS's DynamoDB to store your
     files.
-    
+
     AUTHOR: Brian Mayer
-    
+
     DESCRIPTION: Configuration file for dynasync. Here we store the client's
-    preferences, such as remote db name and tracked directories. 
+    preferences, such as remote db name and tracked directories.
 """
 
-import pathlib
+import os
 import json
 
 # Now we create the .config directory if needed
-config_dir = pathlib.Path().home().joinpath(".config/dynasync")
-if not config_dir.exists():
-    config_dir.mkdir(parents=True)
-    print(".config/dynasync directory created.")
+config_dir = os.path.expanduser("~/.config/dynasync")
+if not os.path.exists(config_dir):
+	os.makedirs(config_dir)
+    print("~/.config/dynasync directory created.")
 
 # Check if config file exists in that dir
-config_file = config_dir.joinpath("dynasync.conf")
+config_file = config_dir + "/dynasync.conf"
 configs = {}
-if config_file.exists():
+if os.path.exists(config_file):
     print("Config file already exists.")
     with open(config_file, 'r') as file:
         configs = json.load(file)
 else:
-    # Create the config file
-    config_file.touch()
-    print("dynasync.conf file created.")
-
     # Get configuration parameters from user and write to file
     configs['dir'] = input("Enter directory to track:")
     configs['table'] = input("Enter DynamoDB table name:")
@@ -37,4 +33,3 @@ else:
         file.write(json.dumps(configs))
 
 print("Configuration finished!")
-
