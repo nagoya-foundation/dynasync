e44e1cd17d805fa239554fa4bd8d5c9090ced746
blmayer <bleemayer@gmail.com>
2018-11-25 15:37:53 -0200

    Adjusted commit function

--- src/push/commit.go
+++ src/push/commit.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 )
 
-func commit(args []string) (int) {
-	skip := 0
+func commit(args []string) {
 	for i := range(args) {
-		if string(args[i][0]) == "-" {
-			fmt.Println("commited file(s)", args[:skip])
-			break
+		if string(args[i]) == "-m" {
+			if i + 1 < len(args) {
+				fmt.Println("commited file(s)", args[1:i])
+				fmt.Println("with message", args[i + 1])
+			} else {
+				fmt.Println("Error: Missing message parameter")
+				showHelp()
+			}
+			return
 		}
-		skip++
 	}
 
-	return skip - 1
+	fmt.Println("Error: Missing parameters")
+	showHelp()
+
+	return
 }
 
