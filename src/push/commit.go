1a127f638a0147f0576bd329a19bc4d0c888f35a
Brian Mayer <bleemayer@gmail.com>
2018-11-29 18:09:09 -0200

    Added file existence verification
    Needs to improve diff file existence check

--- src/push/commit.go
+++ src/push/commit.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"fmt"
+	"os"
+	//"github.com/sergi/go-diff/diffmatchpatch"
 )
 
+func diff(files []string) {
+	// Backup each committed file
+	for _, file := range(files) {
+		fmt.Println("Preparing file " + file + " to commit")
+		_, err := os.Stat(DIFFPATH + "/file-" + file + ".diff")
+
+		// First time committing this file?
+		if err != nil {
+			// Yes, backup the whole file
+			origInfo, err := os.Stat(file)
+			origFile, err := os.Open(file)
+			content := make([]byte, origInfo.Size())
+			_, err = origFile.Read(content)
+
+			if err != nil {
+				panic("Error reading file to commit")
+			}
+
+			diffFile, err := os.Create(DIFFPATH + "/file-" + file + ".orig")
+			_, err = diffFile.Write(content)
+
+			if err != nil {
+				panic("Error copying file to diff folder")
+			}
+			fmt.Println("File backed up")
+		}
+	}
+}
+
 func commit(args []string) {
+	_, err := os.Stat(DIFFPATH)
+	if err != nil {
+		fmt.Println("diff dir not found, make sure you run init first")
+		return
+	}
+
 	for i := range(args) {
 		if string(args[i]) == "-m" {
 			if i + 1 < len(args) {
+				diff(args[1:i])
 				fmt.Println("commited file(s)", args[1:i])
 				fmt.Println("with message", args[i + 1])
 			} else {
@@ -23,4 +61,3 @@ func commit(args []string) {
 
 	return
 }
-
