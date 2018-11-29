ff2baa779e4aecba94e6b8b31e31f5d41991100f
Brian Mayer <bleemayer@gmail.com>
2018-11-29 18:06:12 -0200

    Created global variables
    - Added comments
    - Removed variable used only one time
    - Adjusted for loop arguments

--- src/push/main.go
+++ src/push/main.go
@@ -6,6 +6,12 @@ import(
 	"path/filepath"
 )
 
+// Global variables
+var REPOPATH string
+var SYNCPATH string
+var DIFFPATH string
+
+// TODO: Let function receive argument and return a more detailed help
 func showHelp() {
 	fmt.Println("")
 	fmt.Println("Usage: dynasync [command] file ...")
@@ -19,14 +25,39 @@ func showHelp() {
 	fmt.Println(" name:		set the repo name for name")
 	fmt.Println("")
 	fmt.Println("Needed parameters for commit:")
-	fmt.Println(" file:		file to be sent to repository")
+	fmt.Println(" files:		files to be sent to repository")
 	fmt.Println(" -m message:	commit message")
 }
 
 func initConfig(args []string) {
-	dirPath, _ := os.Getwd()
-	dir := filepath.Base(dirPath)
-	configFile, err := os.Open(dirPath + "/.sync.conf")
+	// Create .sync dir
+	configDir, err := os.Open(SYNCPATH)
+	configDir.Close()
+
+	if err != nil {
+		fmt.Println("Creating config dir")
+		err = os.Mkdir(SYNCPATH, 0777)
+
+		if err != nil {
+			panic("Error creating .sync folder")
+		}
+	}
+
+	// Create .sync/diff dir
+	diffDir, err := os.Open(DIFFPATH)
+	diffDir.Close()
+
+	if err != nil {
+		fmt.Println("Creating diff dir")
+		err = os.Mkdir(DIFFPATH, 0777)
+
+		if err != nil {
+			panic("Error creating diff folder")
+		}
+	}
+
+	// Create .sync/repo.conf file
+	configFile, err := os.Open(SYNCPATH + "/repo.conf")
 	configFile.Close()
 
 	if err == nil {
@@ -34,33 +65,38 @@ func initConfig(args []string) {
 		return
 	}
 
-	configFile, err = os.Create(dirPath + "/.sync.conf")
+	configFile, err = os.Create(SYNCPATH + "/repo.conf")
 	if err != nil {
 		panic("Error creating config file")
 	}
 
 	if len(args) > 1 {
-		_, err = configFile.Write([]byte("repo: " + args[1] + "\n"))
+		_, err = configFile.Write([]byte("name: " + args[1] + "\n"))
 	} else {
-		_, err = configFile.Write([]byte("repo: " + dir + "\n"))
+		_, err = configFile.Write([]byte("name: " + filepath.Base(REPOPATH) + "\n"))
 	}
 
 	if err != nil {
 		panic("Error writing to config file")
 	}
+
+	fmt.Println("Done")
 }
 
 func main() {
+	// Keep track of the repo path 
+	REPOPATH, _ = os.Getwd()
+	SYNCPATH = REPOPATH + "/.sync"
+	DIFFPATH = SYNCPATH + "/diff"
 
-	argLen := len(os.Args)
-	if argLen == 1 {
+	if len(os.Args) == 1 {
 		fmt.Println("Dynasync v1.0.0: A very simple version control system")
 		showHelp()
 		return
 	}
 
-	for i := 1; i < argLen; i++ {
-		switch os.Args[i] {
+	for i, arg := range(os.Args[1:]) {
+		switch arg {
 		case "--help":
 			fallthrough
 		case "help":
@@ -70,13 +106,13 @@ func main() {
 			showHelp()
 			break
 		case "init":
-			initConfig(os.Args[i:])
+			initConfig(os.Args[i + 1:])
 			return
 		case "commit":
-			commit(os.Args[i:])
+			commit(os.Args[i + 1:])
 			return
 		default:
-			fmt.Println("Error: illegal option", os.Args[i])
+			fmt.Println("Error: illegal option", arg)
 			showHelp()
 		}
 	}
