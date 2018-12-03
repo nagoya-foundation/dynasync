f2edd440e63f37d6dc516fda58d024059fad2f71
blmayer <bleemayer@gmail.com>
2018-12-03 19:40:15 -0200

    Added some TODOs

--- src/push/commit.go
+++ src/push/commit.go
@@ -9,14 +9,14 @@ import (
 func diff(files []string) {
 	// Backup each committed file
 	for _, file := range(files) {
-		fmt.Println("Preparing file " + file + " to commit")
+		fmt.Println("preparing file " + file + " to commit")
 		diffFile := DIFFPATH + file + ".orig"
 		diffInfo, errDiff := os.Stat(diffFile)
 		fileInfo, errInfo := os.Stat(file)
 		fileConn, errFile := os.Open(file)
 
 		if errInfo != nil || errFile != nil {
-			panic("Error reading file to commit")
+			panic("error reading file to commit")
 		}
 
 		content := make([]byte, fileInfo.Size())
@@ -25,7 +25,7 @@ func diff(files []string) {
 
 		diffConn, err := os.OpenFile(diffFile, os.O_RDWR|os.O_CREATE, 0666)
 		if err != nil {
-			panic("Error creating diff file")
+			panic("error creating diff file")
 		}
 
 		// First time committing this file?
@@ -37,17 +37,18 @@ func diff(files []string) {
 			dmp := diffmatchpatch.New()
 			patches := dmp.PatchMake(string(diffContent), string(content))
 			diff := dmp.PatchToText(patches)
-			fmt.Println(diff)
+			// TODO: Send patch to DynamoDB
 
 			diffConn.Truncate(0)
-
+		} else {
+			// TODO: Send whole file to DynamoDB
 		}
 
 		_, err = diffConn.WriteAt(content, 0)
 		diffConn.Close()
 
 		if err != nil {
-			panic("Error writing diff file")
+			panic("error writing diff file")
 		}
 	}
 }
@@ -55,7 +56,7 @@ func diff(files []string) {
 func commit(args []string) {
 	_, err := os.Stat(DIFFPATH)
 	if err != nil {
-		fmt.Println("diff dir not found, make sure you run init first")
+		fmt.Println("diff dir not found, make sure you ran init first")
 		return
 	}
 
