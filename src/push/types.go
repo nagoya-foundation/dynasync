2e5d2910ae94fce86c409aa22d6b222c14dc55f6
blmayer <bleemayer@gmail.com>
2019-01-04 19:46:53 -0200

    Added RepoIndex struct, added dynamodb tags

--- src/push/types.go
+++ src/push/types.go
@@ -1,15 +1,23 @@
 package main
 
 type Commit struct {
-	Date    int64    `json:"date"`
-	File    string   `json:"filePath"`
-	Hash    [16]byte `json:"hash"`
-	Diff    string   `json:"diff"`
-	Message string   `json:"message"`
+	Date    int64  `dynamodbav:"commitDate"`
+	File    string `dynamodbav:"filePath"`
+	Author  string `dynamodbav:"author"`
+	Diff    string `dynamodbav:"diff"`
+	Message string `dynamobdav:"message"`
+}
+
+type RepoIndex struct {
+	Repo         string   `dynamodbav:"repo"`
+	Files        []string `dynamodbav:"files,stringset,omitempty"`
+	Commits      []int64  `dynamodbav:"commits,numberset,omitempty"`
+	CreationDate int64    `dynamodbav:"creationDate"`
+	Owner        string   `dynamodbav:"owner"`
 }
 
 type Tag struct {
-	Date int64  `json:"date"`
+//	Date int64  `json:"date"`
 	File string `json:"filePath"`
 	Text string `json:"tag"`
 }
