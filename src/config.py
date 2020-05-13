9c32b8da4106ec2b01de22ef88b261eb0feda685
blmayer <bleemayer@gmail.com>
2020-05-12 22:59:23 -0300

    Moved tables to config file
    - Connecting to database on load function
    - Added function to create config file

--- src/config.py
+++ src/config.py
@@ -15,6 +15,10 @@ _config_file = os.path.expanduser("~/.config/dynasync.conf")
 _dynamo = None
 _profile = ""
 
+index = None
+table = None
+
+
 # ---- Configure ------------------------------------------------------------ #
 
 # Now we create the .config directory if needed
@@ -26,29 +30,42 @@ if not os.path.exists(config_dir):
 def load():
     global index, table
 
-# Check if config file exists in that dir
-config_file = config_dir + "/dynasync.conf"
-if not os.path.exists(config_file) or args.reconf:
-    # Get configuration parameters from user and write to file
-    print("Creating configuration file...")
-    profile = input("Enter the AWS profile name to use: (default)")
-    track_dirs = input("Enter directory to track:")
-    collectionName = input("Enter collection name:")
-
-    if track_dirs == "":
-        print("You must enter a table name and a directory to track")
+    # Check if config file exists in that dir
+    if not os.path.exists(_config_file):
+        print("config file not found in " + _config_file)
+        print("run init to configure")
         exit(1)
+
+    with open(_config_file, 'r') as config_file:
+        _profile = json.load(config_file)['profile']
+
+    session = boto3.Session(profile_name=_profile)
+    _dynamo = session.resource("dynamodb")
+
+    # Table resources
+    index = _dynamo.Table('dynasyncIndex')
+    table = _dynamo.Table('dynasync')
+
+    table_list = [t.name for t in _dynamo.tables.all()]
+    if 'dynasync' not in table_list or 'dynasyncIndex' not in table_list:
+        print("dynasync tables not found, run create_tables first")
+        exit(2)
+
+
+def make():
+    # Get configuration parameters from user and write to file
+    profile = input("Enter the AWS profile name to use: (default)\n")
 
     if profile == "":
         profile = "default"
 
-    with open(config_file, 'w') as file:
-        file.write(json.dumps({
-            'dir':        track_dirs,
-            'collection': collectionName,
-            'profile':    profile
-            })
-        )
+    # Now we create the .config directory if needed
+    config_dir = os.path.expanduser("~/.config")
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+
+    with open(_config_file, 'w') as file:
+        file.write('{"profile": "' + profile + '"}')
 
 with open(config_file, 'r') as file:
     __configs = json.load(file)
