34990216e0835401b5fe7bbfe3c8db4b7d0affb0
blmayer <bleemayer@gmail.com>
2020-05-12 22:57:45 -0300

    Moved loading confi to function

--- src/config.py
+++ src/config.py
@@ -11,10 +11,9 @@ import argparse
 
 # ---- Command line arguments ----------------------------------------------- #
 
-print("Dynasync starting " + time.ctime())
-parser = argparse.ArgumentParser()
-parser.add_argument("--reconf", help="call configure", action="store_true")
-args = parser.parse_args()
+_config_file = os.path.expanduser("~/.config/dynasync.conf")
+_dynamo = None
+_profile = ""
 
 # ---- Configure ------------------------------------------------------------ #
 
@@ -23,6 +22,9 @@ config_dir = os.path.expanduser("~/.config/dynasync")
 if not os.path.exists(config_dir):
     os.makedirs(config_dir)
     print("~/.config/dynasync directory created.")
+
+def load():
+    global index, table
 
 # Check if config file exists in that dir
 config_file = config_dir + "/dynasync.conf"
